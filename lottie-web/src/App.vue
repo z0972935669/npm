18ff1fd77768cfb477cf273249f1739b4ad5ddba
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-11-20 10:29:37 +0800

    update file

---
+++ lottie-web/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div id="bm" class="bm" ref="aaa"></div>
+  </div>
+</template>
+
+<script>
+import lottie from 'lottie-web'
+console.log('lottie :>> ', lottie);
+import bgRipple from '@/assets/json/bg_ripple.json'
+console.log('bgRipple :>> ', bgRipple);
+
+  export default {
+    data() {
+      return {
+        text: 'aaa',
+      };
+    },
+    beforeMount() {
+      
+    },
+    mounted() {
+      this.aaa()
+    },
+    methods: {
+      aaa() {
+        const animation = lottie.loadAnimation({
+          container: document.getElementById('bm'),
+          renderer: 'svg', 
+          loop: false, 
+          autoplay: true,
+          path: 'https://labs.nearpod.com/bodymovin/demo/markus/halloween/markus.json',
+        })
+        animation.addEventListener('complete', function() {
+          animation.destroy()
+          document.getElementById('bm').style.background = '#ff0000'
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.bm {
+  width: 300px;
+  height: 300px;
+}
+</style>
